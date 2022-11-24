c9d0caa36ba587aba5bc5723640eacd9711e461f
Nopalev <faadhilah56@gmail.com>
2022-11-24 11:17:42 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# FP Grafkom
+
+## Sumber
+
+- [3D Modelling](https://github.com/simondevyoutube/ThreeJS_Tutorial_BasicWorld/blob/main/main.js)
+
+- [tata surya](https://www.solarsystemscope.com/textures/)
---
+++ script.js
@@ -0,0 +1,113 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js";
+
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
+
+let scene, camera, renderer;
+let objects = [], rotation = [0.0005, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01];
+
+class Planet {
+	constructor(src, radius, position){
+		const geo = new THREE.SphereGeometry(radius, 64, 32);
+		const texture = new THREE.TextureLoader().load(src);
+		const material = new THREE.MeshBasicMaterial({
+			map: texture
+		});
+		const planet = new THREE.Mesh(geo, material);
+		planet.position.set(position, 0, position);
+		planet.castShadow = false;
+		planet.receiveShadow = true;
+		return planet;
+	}
+}
+
+function init() {
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(
+		55,
+		window.innerWidth / window.innerHeight,
+		45,
+		30000
+	);
+	camera.position.set(-900, 0, 0);
+
+	renderer = new THREE.WebGLRenderer({ 
+		antialias: true 
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	let controls = new OrbitControls(camera, renderer.domElement);
+	controls.addEventListener("change", renderer);
+	controls.minDistance = 50;
+	controls.maxDistance = 2500;
+
+	let skyboxTexture = new THREE.TextureLoader().load("src/space.jpg");
+	skyboxTexture.wrapS = THREE.RepeatWrapping;
+	skyboxTexture.repeat.x = -1;
+	const skyboxMaterial = new THREE.MeshBasicMaterial({
+		map: skyboxTexture
+	});
+	skyboxMaterial.side = THREE.BackSide;
+	let skyboxGeo = new THREE.SphereGeometry(5000, 64, 32);
+	let skybox = new THREE.Mesh(skyboxGeo, skyboxMaterial);
+	objects.push(skybox);
+	scene.add(skybox);
+
+	const sunGeo = new THREE.SphereGeometry(100, 64, 32);
+	const sunTexture = new THREE.TextureLoader().load("src/sun/sun.jpg");
+	const sunMaterial = new THREE.MeshBasicMaterial({
+		map: sunTexture
+	});
+	const sun = new THREE.Mesh(sunGeo, sunMaterial);
+	objects.push(sun);
+	scene.add(sun);
+	
+	const mercury = new Planet("src/mercury/mercury.jpg", 50, 150);
+	objects.push(mercury);
+	scene.add(mercury);
+
+	const venus = new Planet("src/venus/venus.jpg", 50, 250);
+	objects.push(venus);
+	scene.add(venus);
+
+	const earth = new Planet("src/earth/earth.png", 50, 350);
+	objects.push(earth);
+	scene.add(earth);
+
+	const mars = new Planet("src/mars/mars.jpg", 50, 450);
+	objects.push(mars);
+	scene.add(mars);
+
+	const jupiter = new Planet("src/jupiter/jupiter.jpg", 50, 550);
+	objects.push(jupiter);
+	scene.add(jupiter);
+
+	const saturn = new Planet("src/saturn/saturn.jpg", 50, 650);
+	objects.push(saturn);
+	scene.add(saturn);
+
+	const uranus = new Planet("src/uranus/uranus.jpg", 50, 750);
+	objects.push(uranus);
+	scene.add(uranus);
+
+	const neptune = new Planet("src/neptune/neptune.jpg", 50, 850);
+	objects.push(neptune);
+	scene.add(neptune);
+
+	window.addEventListener('resize', () => {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	  }, false);
+
+	animate();
+}
+function animate() {
+	let index = 0;
+	objects.forEach( Element => {
+		Element.rotation.y += rotation[index++];
+	});
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+}
+init();

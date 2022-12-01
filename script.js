7c13edf77c34487610343aaf35e2a16b9fb2aca8
Nopalev <faadhilah56@gmail.com>
2022-12-01 15:25:53 +0700

    added: moon, saturn's ring, and axial tilt of every planet

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.118/build/three.mod
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js";
 
 let scene, camera, renderer;
-let objects = [], 
-	rotation = [0, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01],
-	position = [0, 0, 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000],
-	revolute = [0.0 , 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-	revolutionSpeed = [0.01, 0.009, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.002, 0.001];
+let objects = [], //  [sun, mercury, venus, earth, mars, jupiter, saturn, saturn's ring, uranus, neptune]
+	rotation =        [ 0.01,  0.01,  0.01,  0.01,  0.01,  0.01,  0.01,     0,  0.01,  0.01],
+	position =        [    0,   300,   600,   900,  1200,  1500,  1800,  1800,  2100,  2400],
+	revolute =        [  0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0,   0.0],
+	revolutionSpeed = [    0, 0.008, 0.007, 0.006, 0.005, 0.004, 0.003, 0.003, 0.002, 0.001],
+	moon, moonRevolute = 0.0;
 
 class Planet {
-	constructor(src, radius, position){
+	constructor(src, radius, position, axialTilt){
 		const geo = new THREE.SphereGeometry(radius, 64, 32);
 		const texture = new THREE.TextureLoader().load(src);
 		const material = new THREE.MeshBasicMaterial({
@@ -21,6 +22,7 @@ class Planet {
 		planet.position.set(position, 0, position);
 		planet.castShadow = false;
 		planet.receiveShadow = true;
+		planet.rotation.x = -axialTilt * Math.PI / 180;
 		return planet;
 	}
 }
@@ -55,7 +57,6 @@ function init() {
 	skyboxMaterial.side = THREE.BackSide;
 	let skyboxGeo = new THREE.SphereGeometry(5000, 64, 32);
 	let skybox = new THREE.Mesh(skyboxGeo, skyboxMaterial);
-	objects.push(skybox);
 	scene.add(skybox);
 
 	const sunGeo = new THREE.SphereGeometry(100, 64, 32);
@@ -64,38 +65,54 @@ function init() {
 		map: sunTexture
 	});
 	const sun = new THREE.Mesh(sunGeo, sunMaterial);
+	sun.rotation.x = -7.25 * Math.PI / 180;
 	objects.push(sun);
 	scene.add(sun);
 	
-	const mercury = new Planet("src/mercury/mercury.jpg", 50, 150);
+	const mercury = new Planet("src/mercury/mercury.jpg", 50, 150, 0.03);
 	objects.push(mercury);
 	scene.add(mercury);
 
-	const venus = new Planet("src/venus/venus.jpg", 50, 250);
+	const venus = new Planet("src/venus/venus.jpg", 50, 250, 2.64);
 	objects.push(venus);
 	scene.add(venus);
 
-	const earth = new Planet("src/earth/earth.png", 50, 350);
+	const earth = new Planet("src/earth/earth.png", 50, 350, 23.44);
 	objects.push(earth);
 	scene.add(earth);
 
-	const mars = new Planet("src/mars/mars.jpg", 50, 450);
+	moon = new Planet("src/earth/moon.jpg", 20, 500, 6.68);
+	scene.add(moon);
+
+	const mars = new Planet("src/mars/mars.jpg", 50, 450, 25.19);
 	objects.push(mars);
 	scene.add(mars);
 
-	const jupiter = new Planet("src/jupiter/jupiter.jpg", 50, 550);
+	const jupiter = new Planet("src/jupiter/jupiter.jpg", 50, 550, 3.13);
 	objects.push(jupiter);
 	scene.add(jupiter);
 
-	const saturn = new Planet("src/saturn/saturn.jpg", 50, 650);
+	const saturn = new Planet("src/saturn/saturn.jpg", 50, 650, 26.73);
 	objects.push(saturn);
 	scene.add(saturn);
+	const ringGeo = new THREE.RingGeometry(65, 115, 64);
+	const ringTexture = new THREE.TextureLoader().load("src/saturn/ring.png");
+	const ringMaterial = new THREE.MeshBasicMaterial({
+		map: ringTexture,
+		side: THREE.DoubleSide,
+		transparent: true
+	});
+	const ring = new THREE.Mesh(ringGeo, ringMaterial);
+	ring.rotation.x = Math.PI/2;
+	ring.rotation.x = (90 - 26.73) * Math.PI / 180;
+	objects.push(ring);
+	scene.add(ring);
 
-	const uranus = new Planet("src/uranus/uranus.jpg", 50, 750);
+	const uranus = new Planet("src/uranus/uranus.jpg", 50, 750, 82.23);
 	objects.push(uranus);
 	scene.add(uranus);
 
-	const neptune = new Planet("src/neptune/neptune.jpg", 50, 850);
+	const neptune = new Planet("src/neptune/neptune.jpg", 50, 850, 28.32);
 	objects.push(neptune);
 	scene.add(neptune);
 
@@ -116,6 +133,10 @@ function animate() {
 		revolute[index] += revolutionSpeed[index];
 		index++;
 	});
+	moon.rotation.y += 0.001;
+	moon.position.x = objects[3].position.x + (100 * Math.sin(moonRevolute));
+	moon.position.z = objects[3].position.z + (100 * Math.cos(moonRevolute));
+	moonRevolute += 0.01;
 	renderer.render(scene, camera);
 	requestAnimationFrame(animate);
 }
